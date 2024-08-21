4659b46e2c4dd0324703ffee0d8ed4c7b4e593f1
6410110004_kanokwan <6410110004.psu.ac.th>
2024-08-22 06:36:35 +0700

    finish_profile

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,7 +7,7 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
+          'connect-src': ["'self'", 'https:','http://localhost:1337', 'http://172.20.10.5:1337'],
           'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
           'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
           upgradeInsecureRequests: null,
@@ -15,7 +15,16 @@ module.exports = [
       },
     },
   },
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:1337','http://172.20.10.5:1337'], // เพิ่ม IP ของมือถือ
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      keepHeadersOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
