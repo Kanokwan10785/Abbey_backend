2a1ef81134a3c0076e4e9e9cf700a9d7b00ae22b
Aeknaronk Thanomchon <6410110613@psu.ac.th>
2025-01-18 22:44:43 +0700

    weight-record

---
+++ src/api/weight-record/routes/weight-record.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * weight-record router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::weight-record.weight-record');
---
+++ src/api/weight-record/services/weight-record.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * weight-record service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::weight-record.weight-record');
